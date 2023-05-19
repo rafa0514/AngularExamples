aa9e702ea7e09976f8f8d71e23f23a2535fc5ae0
Rafael Alvarez <rafa_rojopw@hotmail.com>
2023-05-19 13:15:09 -0600

    primeras lecciones angular

--- Ejemplo1/src/app/app.component.ts
+++ Ejemplo1/src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ejemplo1';
+  nombre = 'Rodriguez Pablo';
+  edad = 40;
+  email = 'rpablo@gmail.com';
+  sueldos = [1700, 1600, 1900];
+  activo = true;
+  name = 'Rafael David Alvarez González';
+  sitio = 'http://www.google.com';
+
+  esActivo() {
+    if (this.activo)
+      return 'Trabajador Activo';
+    else
+      return 'Trabajador Inactivo';
+  }
+
+  ultimos3Sueldos() {
+    let suma=0;
+    for(let x=0; x<this.sueldos.length; x++)
+      suma+=this.sueldos[x];
+    return suma;
+  }
 }
